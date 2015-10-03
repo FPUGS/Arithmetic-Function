ebf7ca6ece6f931f44b63740096346f19d597ee9
sgnl <rayrfarias@gmail.com>
2015-10-02 23:49:51 -1000

    implement multiplication functionality; passing tests

--- sgnl/elixir/lib/arithmetic.ex
+++ sgnl/elixir/lib/arithmetic.ex
@@ -2,4 +2,6 @@ defmodule Arithmetic do
   def arith(op1, add, op2) when add == '+', do: op1 + op2
 
   def arith(op1, sub, op2) when sub == '-', do: op1 - op2
+
+  def arith(op1, multi, op2) when multi == '*', do: op1 * op2
 end
