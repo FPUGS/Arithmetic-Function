d2f9a955e1660f17821ccecadb2305f070d1c9a5
sgnl <rayrfarias@gmail.com>
2015-10-02 23:51:25 -1000

    implement division functionality; passing tests

--- sgnl/elixir/lib/arithmetic.ex
+++ sgnl/elixir/lib/arithmetic.ex
@@ -4,4 +4,6 @@ defmodule Arithmetic do
   def arith(op1, sub, op2) when sub == '-', do: op1 - op2
 
   def arith(op1, multi, op2) when multi == '*', do: op1 * op2
+
+  def arith(op1, div, op2) when div == '/', do: op1 / op2
 end
