85df5b6b0d35aa10b3e12c04322e7fb672844069
sgnl <rayrfarias@gmail.com>
2015-10-02 23:46:22 -1000

    add test for subtraction operator

--- sgnl/elixir/test/elixir_test.exs
+++ sgnl/elixir/test/elixir_test.exs
@@ -6,4 +6,9 @@ defmodule ArithmeticTest do
     assert arith(1, '+', 1) == 2
     assert arith(5, '+', 89) == 94
   end
+
+  test "subtract" do
+    assert arith(5, '-', 6) == -1
+    assert arith(54, '-', 32) == 22
+  end
 end
