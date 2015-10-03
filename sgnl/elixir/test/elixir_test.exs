54264f6e8e77723a2d9944821d47f2d87607c68f
sgnl <rayrfarias@gmail.com>
2015-10-02 23:49:29 -1000

    add test for multiplication operator

--- sgnl/elixir/test/elixir_test.exs
+++ sgnl/elixir/test/elixir_test.exs
@@ -4,11 +4,16 @@ defmodule ArithmeticTest do
 
   test "add" do
     assert arith(1, '+', 1) == 2
-    assert arith(5, '+', 89) == 94
+    assert arith(-5, '+', 89) == 84
   end
 
   test "subtract" do
     assert arith(5, '-', 6) == -1
     assert arith(54, '-', 32) == 22
+  end
+
+  test "multiply" do
+    assert arith(7, '*', 10) == 70
+    assert arith(8, '*', -4) == -32
   end
 end
